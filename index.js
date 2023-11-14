1b8b902a2e5b7d896c0f62cd72ae2c88edd4e08d
udaykiran6463 <udaykiran6463@gmail.com>
2023-11-14 11:04:15 +0530

    password genarator first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+
+const copyBtn = document.querySelector("[data-copy]");
+
+const copyMsg = document.querySelector("[data-copyMsg]");
+
+const uppercaseCheck = document.querySelector("#upper-case");
+
+const lowercaseCheck = document.querySelector("#lower-case");
+
+const numberCheck = document.querySelector("#numbers");
+
+const symbolsCheck = document.querySelector("#symbols");
+
+const indicator = document.querySelector("[data-indicator]");
+
+const genarateBtn = document.querySelector(".genarate-btn");
+
+const allCheckBox = document.querySelectorAll("input[type = checkbox]");
+
+
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
+// set strength circle color to gray
+
+lengthDisplay.textContent = passwordLength;
+
+
+
+
+
+
+
+
