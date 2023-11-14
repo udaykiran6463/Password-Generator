946f222df5a6656c5e9e54c868f4d83643984ece
udaykiran6463 <udaykiran6463@gmail.com>
2023-11-14 11:12:05 +0530

    again testing

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const allCheckBox = document.querySelectorAll("input[type = checkbox]");
 
 
 let password = "";
-let passwordLength = 9;
+let passwordLength = 10;
 let checkCount = 1;
 // set strength circle color to gray
 
